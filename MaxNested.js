d1175985174f1866fa305972200fc2156aacaaf0
Jie XU <jie.xu.xj@gmail.com>
2015-12-01 00:37:41 -0600

    Create MaxNested.js

---
+++ MaxNested.js
@@ -0,0 +1,13 @@
+var max;
+if(x > y) {
+	max = x;
+	console.log("x is bigger");
+} else {
+	max = y;
+	if (x == y) {
+		console.log("They are the same");
+	} else {
+		console.log("y is bigger");
+	}
+}
+console.log("max is " + max);
